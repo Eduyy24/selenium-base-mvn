89f5dd0e1b76fb3ead43367db9e59c664168ee47
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2021-09-18 09:25:29 -0500

    class definition and page CheckBox implemented

---
+++ src/test/java/definitions/CheckBoxDefinition.java
@@ -0,0 +1,23 @@
+package definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CheckBoxDefinition {
+    @Given("^Ingreso a la seccion de \"([^\"]*)\"$")
+    public void ingreso_a_la_seccion_de(String arg1) {
+
+    }
+
+
+    @When("^Abro la carpeta home$")
+    public void abro_la_carpeta_home() {
+
+    }
+
+    @Then("^Valido el despliegue de las carpetas Desktop, Documents, Download$")
+    public void valido_el_despliegue_de_las_carpetas_Desktop_Documents_Download() {
+
+    }
+}
---
+++ src/test/resources/features/elements/checkBox.feature
@@ -0,0 +1,12 @@
+@Regresion
+@CriticalRoute
+Feature: Check Box
+
+  @Caso1
+  Scenario: Despliegue de las opciones dentro de la carpeta Home
+    #Given Ingreso a la pagina principal
+    #And Ingreso al modulo de elements
+    And Ingreso a la seccion de "Check Box"
+    When Abro la carpeta home
+    Then Valido el despliegue de las carpetas Desktop, Documents, Download
+
